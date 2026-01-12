7e6e695b0af0e742390a05286ef5b256e73c328b
Musheeb Affan <musheebaffan@Musheebs-MacBook-Air.local>
2026-01-13 00:05:59 +0530

    feat: Updated the props arguments with different datatypes

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,64 @@ function App() {
     <div className="main-container">
       <h1>Laugh - Because you have nothing to do right now</h1>
       <Joke
+        // joke="I told my wife she was drawing her eyebrows too high."
+        punch="No Joke for this - Becasue we are testing conditional rendering"
+        jokeSide="JOKE - "
+        punchSide="PUNCH - "
+        isPun={true}
+        upvotes={2}
+        downvotes={9}
+        comments={["masao", "nice", "funny"]}
+      />
+      <Joke
         joke="I told my wife she was drawing her eyebrows too high."
         punch="She looked surprised."
         jokeSide="JOKE - "
         punchSide="PUNCH - "
+        isPun={true}
+        upvotes={6}
+        downvotes={0}
+        comments={[]}
       />
       <Joke
         joke="Why don't skeletons fight each other?"
         punch="They don't have the guts."
         jokeSide="JOKE - "
         punchSide="PUNCH - "
+        isPun={false}
+        upvotes={5}
+        downvotes={2}
+        comments={["Nice", "Really good", "so funny", "😂😂😂😂😂"]}
       />
       <Joke
         joke="I asked my dog what's 2 minus 2."
         punch="He said nothing."
         jokeSide="JOKE - "
         punchSide="PUNCH - "
+        isPun={true}
+        upvotes={5}
+        downvotes={1}
+        comments={["Hola", "buen", "Nice", "funny"]}
       />
       <Joke
         joke="Parallel lines have so much in common."
         punch="It's a shame they'll never meet."
         jokeSide="JOKE - "
         punchSide="PUNCH - "
+        isPun={true}
+        upvotes={8}
+        downvotes={1}
+        comments={["heyyy", "nova", "activa", "what are you upto? ☺️"]}
       />
       <Joke
         joke="My friend says 'I'm going to become a vegetarian.'"
         punch="I said 'That's a big missed steak.'"
         jokeSide="JOKE - "
         punchSide="PUNCH - "
+        isPun={false}
+        upvotes={9}
+        downvotes={3}
+        comments={["heyy"]}
       />
     </div>
   );
--- src/components/Joke/Joke.jsx
+++ src/components/Joke/Joke.jsx
@@ -1,16 +1,33 @@
 import "./Joke.css";
 
-function Joke({ joke, punch, jokeSide, punchSide }) {
+function Joke({
+  joke,
+  punch,
+  jokeSide,
+  punchSide,
+  upvotes,
+  downvotes,
+  isPun,
+  comments,
+}) {
   return (
     <div>
-      <p className="joke">
-        <span className="joke-side-heading">{jokeSide}</span>
-        {joke}
-      </p>
+      {isPun && <p className="pun">This joke is titled as a Pun</p>}
+      {joke && (
+        <p className="joke">
+          <span className="joke-side-heading">{jokeSide}</span>
+          {joke}
+        </p>
+      )}
       <p className="punch">
         <span className="punch-side-heading">{punchSide}</span>
         {punch}
       </p>
+      <p>Upvotes - {upvotes}</p>
+      <p>Downvotes - {downvotes}</p>
+      <p>Comments - </p>
+      {comments.length > 0 &&
+        comments.map((comment, index) => <p key={index}>{comment}</p>)}
       <hr className="ruler" />
     </div>
   );
